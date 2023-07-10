75bfe98dc1250f65126aa139679c0f5d64f7106b
Aryan Sai <aryansaiarvapelly@gmail.com>
2023-07-10 20:21:25 +0530

    average CPI

--- Asmt1/asmt.c
+++ Asmt1/asmt.c
@@ -32,6 +32,9 @@ void simulate(FILE *inputFile, FILE *outputFile)
   int conditionalCount = 0;
   int takenCount = 0;
   int notTakenCount = 0;
+  int fpAddCount = 0;
+  int fpMulCount = 0;
+  int fpDivCount = 0;
   int fpCount = 0;
 
   int unconditionalCount = 0;
@@ -117,23 +120,30 @@ void simulate(FILE *inputFile, FILE *outputFile)
     {
       unconditionalCount++;
     }
-    else
-    { // arithmetic and others
-      other++;
-    }
-
-    if (strcmp(microOperation, "ADD") == 0 || strcmp(microOperation, "ADD_IMM") == 0 || strcmp(microOperation, "SUB") == 0 || strcmp(microOperation, "SUB_IMM") == 0 || strcmp(microOperation, "SHL") == 0 || strcmp(microOperation, "SHR") == 0 || strcmp(microOperation, "SHL_IMM") == 0 || strcmp(microOperation, "SHR_IMM") == 0)
+    else if (strncmp(microOperation, "ADD", 3) == 0 || strncmp(microOperation, "AND", 3) == 0 || strncmp(microOperation, "MUL", 3) == 0 || strncmp(microOperation, "DIV", 3) == 0 || strcmp(microOperation, "OR") == 0 || strncmp(microOperation, "SUB", 3) == 0 || strcmp(microOperation, "XOR") == 0 || strncmp(microOperation, "SHR", 3) == 0 || strncmp(microOperation, "SHL", 3) == 0 || strncmp(microOperation, "NOT", 3) == 0)
     {
       arithmeticCount++;
     }
-
-    if(strncmp(microOperation,"FP",2)){
+    else if (strcmp(microOperation, "FP_ADD") == 0)
+    {
+      fpAddCount++;
+    }
+    else if (strcmp(microOperation, "FP_MUL") == 0)
+    {
+      fpMulCount++;
+    }
+    else if (strcmp(microOperation, "FP_DIV") == 0)
+    {
+      fpDivCount++;
+    }
+    else if (strncmp(microOperation, "FP", 2) == 0)
+    {
       fpCount++;
     }
-
-    // if(strcmp(microOperation,"JMP_REG")==0 || strcmp(microOperation,"JMP_IMM")==0 ){
-    //   conditionalCount1++;
-    // }
+    else
+    {
+      other++;
+    }
   }
 
   fprintf(outputFile, "Processed %" PRIi64 " trace records.\n\n", totalMicroops);
@@ -141,22 +151,20 @@ void simulate(FILE *inputFile, FILE *outputFile)
   fprintf(outputFile, "Micro-ops: %" PRIi64 "\n", totalMicroops);
   fprintf(outputFile, "Macro-ops: %" PRIi64 "\n\n", totalMacroops);
 
-  fprintf(outputFile, "Arithmetic Instructions: %d \n", arithmeticCount);
-  fprintf(outputFile, "Floating Point Arithmetic Instructions: %d \n", fpCount);
-  fprintf(outputFile, "Load and Store Instructions: %d \n", loadStoreCount);
-  fprintf(outputFile, "Conditional Branches Taken: %d \n", takenCount);
-  fprintf(outputFile, "Conditional Branches Not Taken: %d \n", notTakenCount);
-  fprintf(outputFile, "Unconditional Instructions: %d \n", unconditionalCount);
-  fprintf(outputFile, "Other Instructions: %d \n\n", other-arithmeticCount);
+  fprintf(outputFile, "Arithmetic Instructions: %d (%f %%)\n", arithmeticCount, (float)arithmeticCount * 100 / totalMicroops);
+  fprintf(outputFile, "FP_ADD Instructions: %d (%f %%)\n", fpAddCount, (float)fpAddCount * 100 / totalMicroops);
+  fprintf(outputFile, "FP_MUL Instructions: %d (%f %%)\n", fpMulCount, (float)fpMulCount * 100 / totalMicroops);
+  fprintf(outputFile, "FP_DIV Instructions: %d (%f %%)\n", fpDivCount, (float)fpDivCount * 100 / totalMicroops);
+  fprintf(outputFile, "Other Floating Point Instructions: %d (%f %%)\n", fpCount, (float)fpCount * 100 / totalMicroops);
+  fprintf(outputFile, "Load and Store Instructions: %d (%f %%)\n", loadStoreCount, (float)loadStoreCount * 100 / totalMicroops);
+  fprintf(outputFile, "Conditional Instructions: %d (%f %%)\n", conditionalCount, (float)conditionalCount * 100 / totalMicroops);
+  fprintf(outputFile, "Conditional Branches Taken: %d (%f %%)\n", takenCount, (float)takenCount * 100 / totalMicroops);
+  fprintf(outputFile, "Conditional Branches Not Taken: %d (%f %%)\n", notTakenCount, (float)notTakenCount * 100 / totalMicroops);
+  fprintf(outputFile, "Unconditional Instructions: %d (%f %%)\n", unconditionalCount, (float)unconditionalCount * 100 / totalMicroops);
+  fprintf(outputFile, "Other Instructions: %d (%f %%)\n\n", other, (float)other * 100 / totalMicroops);
 
-  fprintf(outputFile, "Arithmetic Instructions CPI: %d \n", arithmeticCount);
-  fprintf(outputFile, "Floating Point Arithmetic Instructions: %d \n", fpCount);
-  fprintf(outputFile, "Load and Store Instructions CPI: %d \n", loadStoreCount * 2);
-  fprintf(outputFile, "Conditional Branches Taken CPI: %d \n", takenCount * 4);
-  fprintf(outputFile, "Conditional Branches Not Taken CPI: %d \n", notTakenCount * 4);
-  fprintf(outputFile, "Unconditional Instructions CPI: %d \n", unconditionalCount * 2);
-  fprintf(outputFile, "Other Instructions CPI: %d \n", other-arithmeticCount);
-  float avgCPI = ((arithmeticCount) + (loadStoreCount * 2) + (unconditionalCount * 2) + (other-arithmeticCount) + (takenCount * 4) + notTakenCount * 4) / (loadStoreCount + takenCount + notTakenCount + unconditionalCount + other);
+  float avgCPI = (arithmeticCount + loadStoreCount * 4 + unconditionalCount * 2 + conditionalCount * 4 + fpAddCount * 3 + fpDivCount * 30 + fpMulCount * 3) / (arithmeticCount + loadStoreCount + unconditionalCount + conditionalCount + fpAddCount + fpDivCount + fpMulCount);
+
   fprintf(outputFile, "Average CPI: %f \n", avgCPI);
 }
 
